82dfe5f779694be2abc14b390012f734714bf47f
Maryam Azimli <maryam.azimli@ug.bilkent.edu.tr>
2026-04-18 15:33:41 +0300

    camera changes

--- Assets/Scripts/Interaction/Cameramove.cs
+++ Assets/Scripts/Interaction/Cameramove.cs
@@ -7,6 +7,15 @@ public class CameraFollow : MonoBehaviour
 
     void LateUpdate()
     {
+        if (target == null)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if (player != null)
+                target = player.transform;
+
+            return;
+        }
+
         Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
         transform.position = Vector3.Lerp(transform.position, newPos, smooth * Time.deltaTime);
     }
