d3c228bc59a90a4ef625d3b0d1681911621de2ab
Elif Tuğçe Neziroğlu <tugce.neziroglu@ug.bilkent.edu.tr>
2026-04-18 12:16:03 +0300

    Pause and restart system + settings base added

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject pauseMenuPanel;
+    public GameObject pauseWindowPanel;
+    public GameObject settingsPanel;
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuPanel.SetActive(true);
+        pauseWindowPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuPanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Main Menu button clicked");
+    }
+
+    public void OpenSettings()
+    {
+        pauseWindowPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+        pauseWindowPanel.SetActive(true);
+    }
+}
