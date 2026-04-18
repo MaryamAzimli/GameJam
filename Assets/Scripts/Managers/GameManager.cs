1d635cdc47ee25244c0a20d6dd820bed5c70be57
Maryam Azimli <maryam.azimli@ug.bilkent.edu.tr>
2026-04-18 15:22:04 +0300

    Gamemanager edited+texts added

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -3,25 +3,46 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    public int level = 1;
+    public int life = 1;
+
+void Awake()
+{
+    if (instance != null)
+    {
+        Destroy(gameObject);
+        return;
+    }
+
+    instance = this;
+    Debug.Log("Life = " + life);
+    DontDestroyOnLoad(gameObject);
+}
     public void Win()
     {
         Debug.Log("YOU WIN!");
         Invoke("NextLevel", 1f);
     }
-
+   
     public void Lose()
     {
         Debug.Log("YOU LOSE!");
+        life++;
         Invoke("Restart", 1f);
     }
 
     void Restart()
     {
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     void NextLevel()
     {
+        level++;
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
---
+++ Assets/Scripts/Player/LevelText.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+
+public class LevelText : MonoBehaviour
+{
+    public TextMeshProUGUI textUI;
+
+    void Start()
+    {
+        ShowText();
+    }
+    public void ShowText()
+    {
+            if (GameManager.instance == null)
+        return;
+
+        int life = GameManager.instance.life;
+        int level = GameManager.instance.level;
+
+        if (level == 1 && life == 1)
+        {
+            textUI.text = "Drag the screen to move.";
+        }
+        else if (level == 1 && life == 2)
+        {
+            textUI.text = "No logical human being would grab honey with bare hands...";
+        }
+        else if (level == 2)
+        {
+            textUI.text = "Something feels different in this memory...";
+        }
+        else if (life >= 10)
+        {
+            textUI.text = "How many times will you repeat the same mistake?";
+        }
+    }
+}
